65c3b60e5722525b4c99c268951ae8211726aaa1
jlgaoyuan <18904420062@189.cn>
2018-11-02 15:38:38 +0800

    First

---
+++ network/src/main/analysis/format/huawei/router/IpRouTabVpn.java
@@ -0,0 +1,14 @@
+package analysis.format.huawei.router;
+
+import analysis.format.FormatInterface;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpRouTabVpn extends IpRouTab implements FormatInterface {
+    @Override
+    public String run(List<String> list) {
+        return super.run(list);
+    }
+}
