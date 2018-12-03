655937c7bd9b2f9fee6791998a96661b63ebfbea
gaoyuan <18904420062@189.cn>
2018-12-03 15:18:01 +0800

    local

---
+++ learning/src/staticproxy/ProxySubject.java
@@ -0,0 +1,16 @@
+package staticproxy;
+
+/**
+ * Created by jlgaoyuan on 2018/11/26.
+ *
+ */
+public class ProxySubject implements Subject {
+    private Subject subject;
+    public ProxySubject(Subject subject) {
+        this.subject = subject;
+    }
+    @Override
+    public void visit() {
+        subject.visit();
+    }
+}
---
+++ learning/src/staticproxy/RealSubject.java
@@ -0,0 +1,13 @@
+package staticproxy;
+
+/**
+ * Created by jlgaoyuan on 2018/11/26.
+ *
+ */
+public class RealSubject implements Subject {
+    private String name = "byhieg";
+    @Override
+    public void visit() {
+        System.out.println(name);
+    }
+}
