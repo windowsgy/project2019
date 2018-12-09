5dc598b29cff64c5c30da98d78581bba38d37c70
gaoyuan <18904420062@189.cn>
2018-12-09 22:10:56 +0800

    web

--- office/src/sendMail/Load.java
+++ office/src/sendMail/Load.java
@@ -22,6 +22,7 @@ public class Load {
     public static boolean  run(){
         Log.info("Loading Files");
         FileUtils fileUtils = new FileUtils();
+        ListUtils listUtils = new ListUtils();
         BuildParm.summaryFileCode = fileUtils.codeString(BuildParm.SUMMARY_FILEPATH);
         Log.info("Summary File Code :"+BuildParm.summaryFileCode);
         BuildParm.detailFileCode = fileUtils.codeString(BuildParm.DETAIL_FILEPATH);
@@ -31,19 +32,19 @@ public class Load {
         Log.info("Load Summary File To List");
         BuildParm.summaryList = fileUtils.read2List(BuildParm.SUMMARY_FILEPATH,2,BuildParm.detailFileCode);//第二行开始读
         Log.info("Load Summary File To ListArray");
-        BuildParm.summaryListArr = ListUtils.list2ListArray(BuildParm.summaryList, InitParm.SplitStr);
+        BuildParm.summaryListArr = listUtils.list2ListArray(BuildParm.summaryList, InitParm.SplitStr);
         if(BuildParm.summaryListArr == null){
             Log.info("Summary Field Inconformity");//每行字段数量不一致
             return false;
         }
         Log.info("Load Summary Join Field ");
-        BuildParm.summaryJoinField = ListUtils.listArrField(BuildParm.summaryListArr, InitParm.summaryJoinIndex);
+        BuildParm.summaryJoinField = listUtils.listArrField(BuildParm.summaryListArr, InitParm.summaryJoinIndex);
         Log.info("Load Summary Mail Field");
-        BuildParm.summaryMailField = ListUtils.listArrField(BuildParm.summaryListArr, InitParm.detailMailIndex);
+        BuildParm.summaryMailField = listUtils.listArrField(BuildParm.summaryListArr, InitParm.detailMailIndex);
         Log.info("Load SplitFiles Name");
         List<String> splitFileList = fileUtils.getFilesName(BuildParm.SPLIT_SUBPATH);
-        List<String[]> splitFileListArray = ListUtils.list2ListArray(splitFileList,InitParm.FileNameSplitStr);
-        BuildParm.SPLITFILELIST = ListUtils.listArrField(splitFileListArray,0);
+        List<String[]> splitFileListArray = listUtils.list2ListArray(splitFileList,InitParm.FileNameSplitStr);
+        BuildParm.SPLITFILELIST = listUtils.listArrField(splitFileListArray,0);
         Log.info("Loading Files Succeed");
         return true;
     }
